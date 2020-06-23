0cc3db191fc6fd0550034e2af2d204131236351f
Grzegorz Dłużewski <grzes-dluzewski@wp.pl>
2020-06-23 13:48:59 +0200

    First (and hopefully last) commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## The following program is a list of programs to set and get the 
+## matrix and its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        Inv <- NULL
+        set <- function(y) {
+                x <<- y
+                Inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inv) Inv <<- inv
+        getInv <- function() Inv
+        list(set = set,get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
-
+## The following function computes the inverse of the matrix if 
+## it is not yet computed.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        Inv <- x$getInv()
+        if (!is.null(Inv)) {
+                message("getting cached inverse")  
+                return(Inv)
+        }
+        mat <- x$get()
+        Inv <- solve(mat,...)
+        x$setInv(Inv)
+        Inv
 }
